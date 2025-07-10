3db798e2e50952da956c15336fc95860b7e810ad
sonuJha108 <codinghub650@gmail.com>
2025-07-10 17:26:57 +0530

    run time error in this code

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 - for the data base using a sql-lite 
 - Install the 3rd part plugins for using the commands [pip install flask_sqlalchemy]
 
+- create a folder name is model & and also create the file name is model
+- Write the data-base code 
+- Write the data-base connections on app.py file
+- Create the routes on the first URI in the projects
--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect
+from models.model import db,StudentModels
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return 'Hello World!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.before_request
+def create_table():
+    db.create_all()
 
 
-app.run(host='localhost',port=5889)
+@app.route('/create',methods = ['GET','POST'])
+def create():
+    if request.method == 'GET': 
+        return render_template('create.html')
+
+
+# app.run(host='localhost',port=5000)
+
+if __name__ == "__main__":
+    app.run(debug=True)
